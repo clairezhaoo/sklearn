9c47cee0a0098b01576c6aebf36e41a25e1db88b
clairezhaoo <62403358+clairezhaoo@users.noreply.github.com>
2020-10-12 22:51:08 -0700

    Initial commit

---
+++ code.py
@@ -0,0 +1,12 @@
+from sklearn import datasets
+# classifiers
+from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+
+iris = datasets.load_iris()
+digits = datasets.load_digits()
+print(iris)
