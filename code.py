19681720895934d825f52c6b7ba376e0a26f8777
clairezhaoo <62403358+clairezhaoo@users.noreply.github.com>
2020-10-14 22:26:03 -0700

    breast cancer & ridge classifier

--- code.py
+++ code.py
@@ -1,3 +1,5 @@
+# python 3.7
+# Scikit-learn ver. 0.23.2
 from sklearn import datasets
 # classifiers
 from sklearn.linear_model import LogisticRegression
@@ -5,8 +7,22 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import RidgeClassifier
 from sklearn.svm import SVC
+
+from sklearn.datasets import load_iris
+from sklearn.datasets import load_breast_cancer
 
 iris = datasets.load_iris()
 digits = datasets.load_digits()
-print(iris)
+#print(iris)
+"""
+X, y = load_iris(return_X_y=True)
+clf = LogisticRegression(random_state=0).fit(X, y)
+clf.predict(X[:2, :])
+clf.predict_proba(X[:2, :])
+clf.score(X, y)
+"""
+X, y = load_breast_cancer(return_X_y=True)
+clf = RidgeClassifier().fit(X, y)
+print(clf.score(X, y))
