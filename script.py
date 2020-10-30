5ca4289470b38b024d1ae05e319cd4698c22ae77
clairezhaoo <62403358+clairezhaoo@users.noreply.github.com>
2020-10-29 23:56:37 -0700

    classification + clustering

---
+++ script.py
@@ -0,0 +1,132 @@
+from sklearn import datasets
+import numpy as np
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn import neighbors,metrics
+from sklearn.preprocessing import LabelEncoder
+from sklearn import svm
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import scale
+
+standardScaler = StandardScaler()
+
+"""
+# KMeans
+# why does the accuracy go from 7% to 90% ?!?!?
+bc = datasets.load_breast_cancer()
+x = scale(bc.data)  # scaling data, much better
+print(x)
+y = bc.target
+x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=.2)
+# create model
+model = KMeans(n_clusters=2, random_state=0)
+model.fit(x_train)     # clustering only trains labels
+
+predictions = model.predict(x_test)
+labels = model.labels_
+print("Labels: ", labels)
+print("Predictions: ", predictions)
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy: ", accuracy)
+# print(pd.crosstab(y_train, labels))
+"""
+
+
+"""
+data = pd.read_csv("car.data")
+#print(data.head())
+
+X = data[[
+"buying",
+"maint",
+"safety"
+]].values
+y = data[["class"]]
+
+#print(X, y)
+
+# convert data from string to int
+Le = LabelEncoder()
+for i in range(len(X[0])):
+    X[:, i] = Le.fit_transform(X[:, i])
+#print(X)
+
+label_mapping = {
+"unacc": 0,
+"acc": 1,
+"good": 2,
+"vgood": 3
+}
+y["class"] = y["class"].map(label_mapping)
+y = np.array(y)
+#print(y)
+
+
+# KNN model
+knn = neighbors.KNeighborsClassifier(n_neighbors=25, weights='uniform')
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+
+X_train = standardScaler.fit_transform(X_train)
+X_test = standardScaler.transform(X_test)
+
+knn.fit(X_train, y_train)   # train the model
+prediction = knn.predict(X_test)
+accuracy = metrics.accuracy_score(y_test, prediction)
+print("Predictions: ", prediction)
+print("Accuracy: ", accuracy)
+"""
+
+
+"""
+#very accruate: .97-->1
+# SVM
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+classes =["Iris Setosa", "Iris Versiscolour", "Iris Virginica"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+model = svm.SVC()
+model.fit(X_train, y_train)
+print(model)
+predictions = model.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+print("Predictions: ", predictions)
+print("Accuracy: ", accuracy)
+"""
+
+"""
+Attributes of wine dataset
+1) Alcohol
+2) Malic acid
+3) Ash
+4) Alcalinity of ash
+5) Magnesium
+6) Total phenols
+7) Flavanoids
+8) Nonflavanoid phenols
+9) Proanthocyanins
+10)Color intensity
+11)Hue
+12)OD280/OD315 of diluted wines
+13)Proline
+"""
+wine = datasets.load_wine()
+X = wine.data
+print(X)
+y = wine.target
+classes =["class_0", "class_1", "classs_2"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+
+# use standard scaler
+# X_train = standardScaler.fit_transform(X_train)
+# X_test = standardScaler.transform(X_test)
+
+model = svm.SVC()
+model.fit(X_train, y_train)
+print(model)
+predictions = model.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+print("Predictions: ", predictions)
+print("Accuracy: ", accuracy)
